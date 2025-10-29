38d2af2b4b7f71f07f0423cf32d6459a778bdd79
Akshat-1407 <akshat.rastogi1407@gmail.com>
2025-10-29 18:09:33 +0530

    ...

--- hospital/src/Component/Navbar.jsx
+++ hospital/src/Component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-yellow-200 shadow-md sticky top-0 z-50">
+      <nav className="bg-white shadow-md sticky top-0 z-50 ">
         <div className="flex items-center justify-between px-6 md:px-16 py-3">
           {/* Logo */}
           <div className="flex items-center gap-3">
